9dd4e25bf64b9da6558fb0cdd1930e22d138c903
Jack <jackmahonyy@gmail.com>
2017-03-02 14:47:17 +1000

    Uploaded theme folder

---
+++ canva_theme/scripts/defer.js
@@ -0,0 +1,60 @@
+var element = document.createElement("script");
+element.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js";
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+
+
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+//
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].onclick = function(){
+//         /* Toggle between adding and removing the "active" class,
+//         to highlight the button that controls the panel */
+//         this.classList.toggle("active");
+//
+//         /* Toggle between hiding and showing the active panel */
+//         var panel = this.nextElementSibling;
+//         if (panel.style.display === "block") {
+//             panel.style.display = "none";
+//         } else {
+//             panel.style.display = "block";
+//         }
+//     }
+// }
+//
+//
+// $( function() {
+//   $( "#accordion" ).accordion({
+//     collapsible: true
+//   });
+// } );
+//
+// $("#accordion").accordion({ header: "h3", collapsible: true, active: false });
+//
+//
+// jQuery('p:empty').remove();
+//
+//
+// $('a[href^="#"]').click(function(){
+//
+//
+// var the_id = $(this).attr("href");
+//
+//
+//     $('html, body').animate({
+//
+//         scrollTop:$(the_id).offset().top
+//
+//     }, 'slow');
+//
+//
+// return false;});
+//
+//
+// $( function() {
+//   $( "#accordion" ).accordion();
+// } );
